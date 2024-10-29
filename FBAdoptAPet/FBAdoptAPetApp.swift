6e0f9ad58d8c8e616db785a4b3e50205871e01a4
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-10-28 23:41:00 -0500

    Initial Commit

---
+++ FBAdoptAPet/FBAdoptAPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBAdoptAPetApp.swift
+//  FBAdoptAPet
+//
+//  Created by Victor Tabacoff on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FBAdoptAPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
